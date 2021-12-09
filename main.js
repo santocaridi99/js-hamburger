48c3c5e082b985c50de4d9153cd2f8f57a97f633
santocaridi99 <santocaridi99@gmail.com>
2021-12-09 17:28:44 +0100

    risolto usando le classi

--- main.js
+++ main.js
@@ -4,13 +4,13 @@ const button = document.querySelector(".fa-bars");
 //dichiaro la cost menu che mi servirà una volta cliccato il button per cambiare il suo display
 const menu = document.querySelector(".hamburger-menu");
 button.addEventListener('click', function() {
-   menu.style.display ="block";
+   menu.classList.add('active');
 })
 //chiusura quando clicco close
 //dichiaro classe closeMenu che sta per il link con class close
 //ricordando che il menu si dovrà chiudere
 const closeMenu = document.querySelector(".close");
 closeMenu.addEventListener('click',function(){
-    menu.style.display="none";
+    menu.classList.remove('active');
 })
 
