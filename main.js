413c2ea52db7eb5c4e27a200b210880dd6c1073e
santocaridi99 <santocaridi99@gmail.com>
2021-12-09 16:05:43 +0100

    fine esercizio

---
+++ main.js
@@ -0,0 +1,16 @@
+//apertura
+//dichiaro la const button che sta per il carattere speciale a forma di bottone
+const button = document.querySelector(".fa-bars");
+//dichiaro la cost menu che mi servirà una volta cliccato il button per cambiare il suo display
+const menu = document.querySelector(".hamburger-menu");
+button.addEventListener('click', function() {
+   menu.style.display ="block";
+})
+//chiusura quando clicco close
+//dichiaro classe closeMenu che sta per il link con class close
+//ricordando che il menu si dovrà chiudere
+const closeMenu = document.querySelector(".close");
+closeMenu.addEventListener('click',function(){
+    menu.style.display="none";
+})
+
